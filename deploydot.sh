0d0fd3b00fb33c3cf44aded01d407700382d8a85
Brian Davis <bpdavis14@gmail.com>
2020-06-18 09:19:09 -0400

    Added ignore list to config. Updated deployment for added files

--- deploydot.sh
+++ deploydot.sh
@@ -4,3 +4,5 @@
 ln -s $(pwd)/.bashrc ~/.bashrc
 ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+ln -s $(pwd)/.gitconfig ~/.gitconfig
+ln -s $(pwd)/.gitignore_global ~/.gitignore_global
