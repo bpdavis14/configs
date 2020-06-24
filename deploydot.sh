9baa5550977597f4bd8f3a80422b17c34e8d100e
Brian Davis <bpdavis14@gmail.com>
2020-06-24 10:43:30 -0400

    Updated deployment to clear default dotfiles

--- deploydot.sh
+++ deploydot.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+# Delete any default configuration files
+if [[ -f ~/.bashrc ]]; then
+    rm ~/.bashrc
+fi
+if [[ -f ~/.vimrc ]]; then
+    rm ~/.vimrc
+fi
+if [[ -f ~/.tmux.conf ]]; then
+    rm ~/.tmux.conf
+fi
+if [[ -f ~/.gitconfig ]]; then
+    rm ~/.gitconfig
+fi
+if [[ -f ~/.gitignore_global ]]; then
+    rm ~/.gitignore_global
+fi
+
 # Automatically deploy my custom dotfiles when ran
 ln -s $(pwd)/.bashrc ~/.bashrc
 ln -s $(pwd)/.vimrc ~/.vimrc
